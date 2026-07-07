052dcc09978e76a31213add0c617c9835f488764
Ehtisham <ehtishamiqbal433@gmail.com>
2026-07-07 20:07:06 +0300

    code hardening and API playground were added.

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'BCs-IRZcchgM8M60HYyYu0iZfolKQqNV',
+            'X-App-Proxy': '8CMYSAjOhHjPpQjDanPbseRiz3WqUgya',
           },
           body: JSON.stringify(requestDetails),
         };
